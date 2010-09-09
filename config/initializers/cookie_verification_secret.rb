787a85ca0bb27c8130948fa46cf8f6d55b6e5b69
Robin <wang_haibing@126.com>
2010-09-09 17:35:08 +0800

    initial add

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '213ab4f785bcab0f678e7eac7f142920e04f990715dfc19b4736ca335873f41590386f702d0035770adb7dd0c9b3fc12e9ed395a1a9c4cf620fd4f91a5806f16';
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resource :account, :controller => "users"
+  map.resources :users
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new"
+  
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+end
