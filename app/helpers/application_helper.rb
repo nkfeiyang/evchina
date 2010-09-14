c508de0cc885cadcc5cc10e77bbdef94b8dbcc62
nkfeiyang <wang_haibing@126.com>
2010-09-14 23:29:17 +0800

    update bat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def title
-    base_title = "活动网"
+    base_title = "优活网"
     if @title.nil?
       base_title
     else
