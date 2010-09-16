7973a2189fdb8fc8585ae72ae5f8a2ea64c33cb1
Robin <wang_haibing@126.com>
2010-09-16 18:25:46 +0800

    update bat

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @user_session = UserSession.new
   end
 
   def contact
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def title
-    base_title = "优活网"
+    base_title = "优客网"
     if @title.nil?
       base_title
     else
